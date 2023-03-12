39de8727c911c8bafee7dabd6651c4250b3b1e95
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-03-12 13:47:36 -0700

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.getElementById('main');
+let newHeader = document.createElement('h1');
+
+main.remove();
+
+document.body.append(newHeader.id='victory', newHeader.innerHTML = 'Alden is the champion');
+
